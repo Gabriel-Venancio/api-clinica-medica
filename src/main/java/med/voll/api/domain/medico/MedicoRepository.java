4e72f2c9e29e0d4aed16888d845eeb5ed3d3f88c
Gabriel Venancio <Gabriel@DESKTOP-QQFFJ0U>
2023-08-11 17:16:32 -0300

    Initial commit

---
+++ src/main/java/med/voll/api/domain/medico/MedicoRepository.java
@@ -0,0 +1,30 @@
+package med.voll.api.domain.medico;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDateTime;
+
+public interface MedicoRepository extends JpaRepository<Medico, Long> {
+    Page<Medico> findAllByAtivoTrue(Pageable paginacao);
+
+    @Query("SELECT m FROM Medico m " +
+            "WHERE m.ativo = 1 " +
+            "AND m.especialidade = :especialidade " +
+            "AND m.id NOT IN (" +
+            "    SELECT c.medico.id FROM Consulta c " +
+            "    WHERE c.data = :data) " +
+            "ORDER BY FUNCTION('RAND') " +
+            "LIMIT 1")
+    Medico escolherMedicoAleatorioLivreNaData(Especialidade especialidade, LocalDateTime data);
+
+    @Query("""
+            select m.ativo
+            from Medico m
+            where
+            m.id = :id
+            """)
+    Boolean findAtivoById(Long id);
+}
---
+++ src/main/java/med/voll/api/infra/security/DadoTokenJWT.java
@@ -0,0 +1,5 @@
+package med.voll.api.infra.security;
+
+public record DadoTokenJWT(String token) {
+
+}
